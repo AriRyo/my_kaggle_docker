c33f3b74caa6a8a175b1b9b080c37a8025c0beb4
AriRyo <come11926323@gmail.com>
2024-04-03 01:12:17 +0900

    fix:jupyter notebookからjupyter labに変更

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 
-jupyter notebook --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.token="kaggle" --notebook-dir=/kaggle
+# jupyter notebook --ip=0.0.0.0 --allow-root --no-browser --NotebookApp.token="098098" --notebook-dir=/kaggle
+jupyter lab --ip=0.0.0.0 --allow-root --LabApp.token="098098" --no-browser
